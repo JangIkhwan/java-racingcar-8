ecd7d8d14567314c931921a24b57b0e51446782f
JangIkhwan <bluensky0213@gmail.com>
2025-10-25 21:39:05 +0900

    feat(RacingCar) : 자동차 클래스 구현
    - 경주에서 자동차의 상태를 관리하는 자동차 클래스 구현
    - 전진하기 메서드와 getter 메서드 작성

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,25 @@
+package racingcar.model;
+
+import racingcar.model.dto.CarState;
+
+public class RacingCar {
+    private String name;
+    private int distance;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void goForward() {
+        this.distance++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+}
