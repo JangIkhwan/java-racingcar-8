71c1f2d8d97a2bba407c20310e3e5fbdf3c246ec
JangIkhwan <bluensky0213@gmail.com>
2025-10-25 22:38:01 +0900

    feat(Application) : 컨트롤러 의존성 주입 및 메서드 실행
    - 컨트롤러에 필요한 의존성을 주입
    - 컨트롤러의 메서드 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.view.RacingInputParser;
+import racingcar.view.RacingInputView;
+import racingcar.view.RacingOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingInputParser inputParser = new RacingInputParser();
+        RacingInputView inputView = new RacingInputView(inputParser);
+        RacingOutputView outputView = new RacingOutputView();
+        RacingController controller = new RacingController(inputView, outputView);
+        controller.run();
     }
 }
