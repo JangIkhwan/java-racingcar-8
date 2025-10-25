8c5c0d19d64b0894e90151e5ca6962301d94a1de
JangIkhwan <bluensky0213@gmail.com>
2025-10-25 22:05:49 +0900

    feat(RacingCar) : 자동차 클래스에 getter 추가
    - 이름과 거리를 반환하는 getter 추가

--- src/main/java/racingcar/view/dto/RacingInput.java
+++ src/main/java/racingcar/view/dto/RacingInput.java
@@ -5,8 +5,17 @@ import java.util.List;
 public class RacingInput {
     private final List<String> carNames;
     private final int moveNumber;
+
     public RacingInput(List<String> carNames, int moveNumber) {
         this.carNames = carNames;
         this.moveNumber = moveNumber;
+    }
+
+    public List<String> getCarNames() {
+        return this.carNames;
+    }
+
+    public int getMoveNumber(){
+        return this.moveNumber;
     }
 }
