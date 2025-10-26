687f068596acec014f95e2d75dae153e4f3fedef
JangIkhwan <bluensky0213@gmail.com>
2025-10-27 01:22:46 +0900

    feat(CarNameList) : 자동차 리스트 구현
    - 자동차들을 관리하고 자동차들을 전진시키는 객체 구현

---
+++ src/main/java/racingcar/model/RacingCarList.java
@@ -0,0 +1,55 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.model.dto.CarName;
+import racingcar.model.dto.CarNameList;
+import racingcar.model.dto.CarState;
+import racingcar.model.dto.GameState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarList {
+    private List<RacingCar> racingCars = new ArrayList<>();
+
+    public RacingCarList(CarNameList carNameList){
+        if(carNameList.length() <= 0){
+            throw new IllegalArgumentException("자동차는 1대 이상이어야 합니다.");
+        }
+        for(CarName name : carNameList.getCarNames())
+            this.racingCars.add(new RacingCar(name));
+    }
+
+    public void moveCars() {
+        for(RacingCar car : racingCars){
+            int rand = Randoms.pickNumberInRange(0, 9);
+            if(rand >= 4) {
+                car.goForward();
+            }
+        }
+    }
+
+    public GameState getState(){
+        List<CarState> carStates = new ArrayList<>();
+        for(RacingCar car : racingCars){
+            carStates.add(new CarState(car.getName(), car.getDistance()));
+        }
+        return new GameState(carStates);
+    }
+
+    public List<String> getWinnerNames(){
+        int maxDistance = 0;
+        List<String> winnerNames = new ArrayList<>();
+        for(RacingCar car : racingCars){
+            if(maxDistance < car.getDistance()){
+                winnerNames = new ArrayList<>();
+                winnerNames.add(car.getName());
+                maxDistance = car.getDistance();
+            }
+            else if (maxDistance == car.getDistance()){
+                winnerNames.add(car.getName());
+            }
+        }
+        return winnerNames;
+    }
+}
