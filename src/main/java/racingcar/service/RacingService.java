770e263d14034f4c38a35b24b6972c03a2f9f413
JangIkhwan <bluensky0213@gmail.com>
2025-10-27 01:36:33 +0900

    feat(RacingService) : 자동차 경주 서비스 구현
    - 게임 객체를 생성하고 실행하는 역할을 하는 서비스 구현

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -5,12 +5,6 @@ import racingcar.model.dto.GameResult;
 import racingcar.view.dto.RacingInput;
 
 public class RacingService {
-    private final RacingGame game;
-
-    public RacingService(RacingGame game){
-        this.game = game;
-    }
-
     public GameResult race(RacingInput input){
         RacingGame game = new RacingGame(input.getCarNameList(), input.getMoveNumber());
         return game.play();
