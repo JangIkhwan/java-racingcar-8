3fdffc72e500ffd87aa37fa6e0542a44042dc758
JangIkhwan <bluensky0213@gmail.com>
2025-10-27 01:23:36 +0900

    refactor(RacingGame) : 자동차 리스트를 적용하여 수정
    - 자동차 리스트를 적용해서 게임 객체를 수정

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,62 +1,25 @@
 package racingcar.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.dto.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
-    private List<RacingCar> cars = new ArrayList<>();
+    private RacingCarList carList;
     private int moveNumber;
 
     public RacingGame(CarNameList carNameList, MoveNumber moveNumber){
-        this.cars = carNameList.getCarNames().stream()
-                .map(name -> new RacingCar(name))
-                .toList();
-
+        this.carList = new RacingCarList(carNameList);
         this.moveNumber = moveNumber.getValue();
     }
 
     public GameResult play() {
         List<GameState> gameStates = new ArrayList<>();
         for(int move = 0; move < moveNumber; move++){
-            moveCars();
-            gameStates.add(getState());
+            carList.moveCars();
+            gameStates.add(carList.getState());
         }
-        return new GameResult(gameStates, getWinnerNames());
-    }
-
-    private void moveCars() {
-        for(RacingCar car : cars){
-            int rand = Randoms.pickNumberInRange(0, 9);
-            if(rand >= 4) {
-                car.goForward();
-            }
-        }
-    }
-
-    private GameState getState(){
-        List<CarState> carStates = new ArrayList<>();
-        for(RacingCar car : cars){
-            carStates.add(new CarState(car.getName(), car.getDistance()));
-        }
-        return new GameState(carStates);
-    }
-
-    private List<String> getWinnerNames(){
-        int maxDistance = 0;
-        List<String> winnerNames = new ArrayList<>();
-        for(RacingCar car : cars){
-            if(maxDistance < car.getDistance()){
-                winnerNames = new ArrayList<>();
-                winnerNames.add(car.getName());
-                maxDistance = car.getDistance();
-            }
-            else if (maxDistance == car.getDistance()){
-                winnerNames.add(car.getName());
-            }
-        }
-        return winnerNames;
+        return new GameResult(gameStates, carList.getWinnerNames());
     }
 }
