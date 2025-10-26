a4103028675b5f62e7b67998e4945c2a5ec516cb
JangIkhwan <bluensky0213@gmail.com>
2025-10-27 00:19:04 +0900

    feat(CarNameList) : 자동차 이름 리스트 구현
    - 자동차의 이름을 중복이 없도록 관리하는 리스트 구현

---
+++ src/main/java/racingcar/model/dto/CarNameList.java
@@ -0,0 +1,23 @@
+package racingcar.model.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarNameList {
+    private List<CarName> carNames;
+
+    public CarNameList(){
+        carNames = new ArrayList<>();
+    }
+
+    public void add(CarName name){
+        if(carNames.contains(name)){
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
+        carNames.add(name);
+    }
+
+    public List<CarName> getCarNames(){
+        return carNames;
+    }
+}
