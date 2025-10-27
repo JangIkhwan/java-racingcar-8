74b1f2feb501a0ca72b49940b7567f3145992608
JangIkhwan <bluensky0213@gmail.com>
2025-10-27 14:36:43 +0900

    test(RacingCarTest) : 자동차 클래스 테스트 코드 추가
    - 자동차 클래스에 새로 추가한 메서드들에 대한 테스트 코드 추가

--- src/test/java/racingcar/model/RacingCarTest.java
+++ src/test/java/racingcar/model/RacingCarTest.java
@@ -3,6 +3,7 @@ package racingcar.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.dto.CarName;
+import racingcar.model.dto.CarState;
 import racingcar.model.strategy.CarMovingStrategyImpl;
 
 import static org.assertj.core.api.Assertions.*;
@@ -21,5 +22,48 @@ class RacingCarTest {
 
         // then
         assertThat(car.getDistance()).isEqualTo(1);
+    }
+
+    @DisplayName("먼 거리를 달린 자동차가 이긴다")
+    @Test
+    void shouldWinWhenDistanceIsLongerThanAnothers(){
+        // given
+        RacingCar one = new RacingCar(new CarName("car1"), () -> true);
+        RacingCar another = new RacingCar(new CarName("car2"), () -> true);
+
+        one.move();
+
+        // when & then
+        assertThat(one.win(another)).isTrue();
+    }
+
+    @DisplayName("같은 거리를 달린 자동차는 서로 비긴다")
+    @Test
+    void shouldDrawWhenDistanceIsSameAsAnothers(){
+        // given
+        RacingCar one = new RacingCar(new CarName("car1"), () -> true);
+        RacingCar another = new RacingCar(new CarName("car2"), () -> true);
+
+        one.move();
+        another.move();
+
+        // when & then
+        assertThat(one.draw(another)).isTrue();
+    }
+
+    @DisplayName("상태를 반환한다")
+    @Test
+    void shouldReturnState(){
+        // given
+        RacingCar car = new RacingCar(new CarName("car1"), () -> true);
+
+        car.move();
+
+        // when
+        CarState state = car.getState();
+
+        // then
+        assertThat(state.getName()).isEqualTo("car1");
+        assertThat(state.getDistance()).isEqualTo(1);
     }
 }
