8aaa4f75d6f7fc33f53f3a023cff4a21d6d0cced
JangIkhwan <bluensky0213@gmail.com>
2025-10-27 15:11:25 +0900

    fix(ApplicationTest) : 통합 테스트에 케이스 추가
    - 통합 테스트에 성공 케이스와 실패 케이스를 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -28,6 +31,62 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("두 개의 자동차가 겨루어도 정상 동작한다")
+    @Test
+    void shouldRunWhenTwoCarsAreGiven() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "2");
+                    assertThat(output()).contains("pobi : -", "woni : -", "pobi : --", "woni : --", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @DisplayName("세 개의 자동차가 겨루어도 정상 동작한다")
+    @Test
+    void shouldRunWhenThreeCarsAreGiven() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,hwani", "3");
+                    assertThat(output()).contains("pobi : -", "woni : -", "hwani : -", "pobi : --", "최종 우승자 : pobi");
+                    assertThat(output()).doesNotContain("woni : --", "hwani : --");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, STOP, STOP
+        );
+    }
+
+    @DisplayName("자동차 이름의 길이가 1자 미만이거나 5자를 초과하면 벗어나면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdef,abc", ",abc", "", ",", "abc,"})
+    void shouldThrowExceptionWhenCarNameIsInvalid(String names){
+        assertSimpleTest(()->
+                assertThatThrownBy(() -> runException(names, "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("중복되는 자동차 이름이 존재하면 예외가 발생한다")
+    @Test
+    void shouldThrowExceptionWhenDuplicateNameExists(){
+        assertSimpleTest(()->
+            assertThatThrownBy(() -> runException("abc,abc", "1"))
+                    .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("전진 횟수가 자연수가 아니면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"3.14", "하나", "1개", "-1"})
+    void shouldThrowExceptionWhenMoveNumberIsNotInteger(String number){
+        assertSimpleTest(()->
+            assertThatThrownBy(() -> runException("abc,abcd", number))
+                    .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
